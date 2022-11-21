93fdd614ebfe1497b68556a775fdb1f80010a7bf
crismgsp <crismgsp@gmail.com>
2022-11-21 15:09:35 -0300

    inserindo condicao pra so aparecer tabela caso tenha os dados suspeitos pra este mes

--- app/sistema/Views/analiseTransacoes.php
+++ app/sistema/Views/analiseTransacoes.php
@@ -40,102 +40,127 @@ if(isset($this->data['form'])) {
 
 </form>
 
+<?php
 
 
-</div>
-
-<h3 style="text-align:center;">Transações Suspeitas</h3>
-
-<table class="table" style="border: 1px solid blue;" >
-    <thead>
-        <tr>
-            <th scope="col">Banco de Origem</th>
-            <th scope="col">Agencia de Origem</th>
-            <th scope="col">Conta de Origem</th>
-            <th scope="col">Banco de Destino</th>
-            <th scope="col">Agencia de Destino</th>
-            <th scope="col">Conta de Destino</th>
-            <th scope="col">Valor</th>  
-            <th scope="col">Data e Hora da transação</th>
-            <th scope="col">Data e Hora da Importação</th>
-            <th scope="col">Mes</th>
+$contaSuspeita = $this->data['contasuspeita'];
+$agenciaSuspeita = $this->data['agenciasuspeita'];
+$transacaoSuspeita = $this->data['transacaosuspeita'];
 
 
-        </tr>
-    </thead>
-    <tbody>
-        
-        
-        <?php foreach ($this->data['transacaosuspeita'] as $contas) : ?>
-            <tr>
-                <td><?php echo $contas['BancoOrigem']; ?></td>
-                <td><?php echo $contas['AgenciaOrigem']; ?></td>
-                <td><?php echo $contas['ContaOrigem']; ?></td>
-                <td><?php echo $contas['BancoDestino']; ?></td>
-                <td><?php echo $contas['AgenciaDestino']; ?></td>
-                <td><?php echo $contas['ContaDestino']; ?></td>
-                <td><?php echo $contas['Valor']; ?></td>
-                <td><?php echo $contas['DataeHora']; ?></td>
-                <td><?php echo $contas['DataHoraImportacao']; ?></td>
-                <td><?php echo $contas['Mes']; ?></td>
-                
+if(isset($_POST['enviaMes'])){ 
+    if(!empty($transacaoSuspeita)){?>
+        </div>
 
-            </tr>
-        <?php endforeach; ?> 
-    </tbody>
-</table>
+        <h3 style="text-align:center;">Transações Suspeitas</h3>
 
-<br>
-
-<h3 style="text-align:center; ">Contas Suspeitas</h3>
-<table class="table" style="border: 1px solid blue;">
-    <thead>
-        <tr>
-            <th scope="col">Banco</th>
-            <th scope="col">Agencia</th>
-            <th scope="col">Conta</th>
-            <th scope="col">Valor</th> 
-            
+        <table class="table" style="border: 1px solid blue;" >
+            <thead>
+                <tr>
+                    <th scope="col">Banco de Origem</th>
+                    <th scope="col">Agencia de Origem</th>
+                    <th scope="col">Conta de Origem</th>
+                    <th scope="col">Banco de Destino</th>
+                    <th scope="col">Agencia de Destino</th>
+                    <th scope="col">Conta de Destino</th>
+                    <th scope="col">Valor</th>  
+                    <th scope="col">Data e Hora da transação</th>
+                    <th scope="col">Data e Hora da Importação</th>
+                    <th scope="col">Mes</th>
 
 
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($this->data['contasuspeita']  as $contames) : ?>
-            <tr>
-                <td><?php echo $contames['Banco']; ?></td>
-                <td><?php echo $contames['Agencia'];  ?></td>
-                <td><?php echo $contames['Conta']; ?></td>
-                <td><?php echo $contames['Soma']; ?></td>
-                
-            </tr>
-        <?php endforeach; ?> 
-    </tbody>
-</table>
-
-<br>
-
-<h3 style="text-align:center;">Agências Suspeitas</h3>
-<table class="table" style="border: 1px solid blue;" >
-    <thead>
-        <tr>
-            <th scope="col">Banco</th>
-            <th scope="col">Agencia</th>
-            <th scope="col">Valor</th> 
-
-
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($this->data['agenciasuspeita'] as $agencia) : ?>
-            <tr>
-                <td><?php echo $agencia['Banco']; ?></td>
-                <td><?php echo $agencia['Agencia']; ?></td>
-                <td><?php echo $agencia['Soma']; ?></td>
+                </tr>
+            </thead>
+            <tbody>
                 
                 
+                <?php foreach ($this->data['transacaosuspeita'] as $contas) : ?>
+                    <tr>
+                        <td><?php echo $contas['BancoOrigem']; ?></td>
+                        <td><?php echo $contas['AgenciaOrigem']; ?></td>
+                        <td><?php echo $contas['ContaOrigem']; ?></td>
+                        <td><?php echo $contas['BancoDestino']; ?></td>
+                        <td><?php echo $contas['AgenciaDestino']; ?></td>
+                        <td><?php echo $contas['ContaDestino']; ?></td>
+                        <td><?php echo $contas['Valor']; ?></td>
+                        <td><?php echo $contas['DataeHora']; ?></td>
+                        <td><?php echo $contas['DataHoraImportacao']; ?></td>
+                        <td><?php echo $contas['Mes']; ?></td>
+                        
 
-            </tr>
-        <?php endforeach; ?> 
-    </tbody>
-</table>
+                    </tr>
+                <?php endforeach; ?> 
+            </tbody>
+        </table>
+
+        <?php }else{
+            echo "Este mês selecionado não tem transações suspeitas";
+        }   
+    }?> 
+    
+<?php
+    
+if(isset($_POST['enviaMes'])){    
+    if(!empty($contaSuspeita)){?>
+
+        <h3 style="text-align:center; ">Contas Suspeitas</h3>
+        <table class="table" style="border: 1px solid blue;">
+            <thead>
+                <tr>
+                    <th scope="col">Banco</th>
+                    <th scope="col">Agencia</th>
+                    <th scope="col">Conta</th>
+                    <th scope="col">Valor</th> 
+                    
+
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($this->data['contasuspeita']  as $contames) : ?>
+                    <tr>
+                        <td><?php echo $contames['Banco']; ?></td>
+                        <td><?php echo $contames['Agencia'];  ?></td>
+                        <td><?php echo $contames['Conta']; ?></td>
+                        <td><?php echo $contames['Soma']; ?></td>
+                        
+                    </tr>
+                <?php endforeach; ?> 
+            </tbody>
+        </table>
+    <?php }else{
+        echo "Este mês selecionado não tem conta suspeita";
+    }  
+}            
+ 
+
+if(isset($_POST['enviaMes'])){ 
+    if(!empty($agenciaSuspeita)){?>    
+        <h3 style="text-align:center;">Agências Suspeitas</h3>
+        <table class="table" style="border: 1px solid blue;" >
+            <thead>
+                <tr>
+                    <th scope="col">Banco</th>
+                    <th scope="col">Agencia</th>
+                    <th scope="col">Valor</th> 
+
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($this->data['agenciasuspeita'] as $agencia) : ?>
+                    <tr>
+                        <td><?php echo $agencia['Banco']; ?></td>
+                        <td><?php echo $agencia['Agencia']; ?></td>
+                        <td><?php echo $agencia['Soma']; ?></td>
+                        
+                        
+
+                    </tr>
+                <?php endforeach; ?> 
+            </tbody>
+        </table>
+    <?php }else{
+        echo "Este mes selecionado não tem agencia suspeita";
+    }
+} ?>    
